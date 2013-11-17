b183c785188f6193655e86087c6bb9de7e8edd7c
pchr <philipp.christen@gmail.com>
2013-11-17 15:39:10 +0100

    Hide registerArea by clicking on X

--- WebContent/resources/javascript/js.js
+++ WebContent/resources/javascript/js.js
@@ -9,13 +9,15 @@ $( document ).ready(function() {
 	
 	// Show register area on btn-Click
 	$("#registerBtn").click( function() {
-		$("#register").show();
+		showRegisterArea();
 	} );
 	
 	// This function checks if all fields are filled before enabling the sumit-Button.
 	$( "#addNewLink input[type=text]" ).keyup( function() {
 		if( fieldsAreFilled() ) {
 			$("#addNewLink input.submitBtn").removeAttr('disabled').removeClass( 'ui-state-disabled' );
+		} else {
+			$("#addNewLink input.submitBtn").attr('disabled', 'disabled' ).addClass( 'ui-state-disabled' );
 		}
 	});
 	
@@ -23,6 +25,11 @@ $( document ).ready(function() {
 	// This will also show the "submit new link" button in the header again.
 	$("#cancelSubmit").click( function() {
 		hideAddLinkArea();
+	} );
+	
+	// Hide the Register area on click on the cancel-Button
+	$("#cancelRegister").click( function() {
+		hideRegisterArea();
 	} );
 	
 	
@@ -39,8 +46,13 @@ $( document ).ready(function() {
 		$("#addNewLink").show();
 		$("#addNewLinkBtn").hide();
 	}
+	function hideRegisterArea() {
+		$("#register").hide();
+	}
+	function showRegisterArea() {
+		$("#register").show();
+	}
 	function fieldsAreFilled() {
-		// TODO: check if all fields are filled.
 		var $emptyFields = $( "#addNewLink input[type=text]" ).filter(function() {
             return $.trim(this.value) === "";
         });
